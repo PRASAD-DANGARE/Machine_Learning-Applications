079bd521072e72fe303571689dddb51154bfbdb0
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-05-05 19:48:19 +0530

    This File Contains Modules For Marvellous.py File

---
+++ Application6/Mymodules.py
@@ -0,0 +1,10 @@
+'''
+
+This Mymodules.py File Contain Modules For Marvellous.py File 
+
+'''
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from scipy.spatial import distance
+from sklearn.metrics import accuracy_score
